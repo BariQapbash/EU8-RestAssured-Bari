1d37b3056efe82f9722f6abe152ccb677318cc43
BariQapbash <99431864+BariQapbash@users.noreply.github.com>
2022-07-25 16:04:49 +0200

    day05 continue

---
+++ src/test/java/com/cybertek/day05/HamcrestMatchersApiTest.java
@@ -0,0 +1,52 @@
+package com.cybertek.day05;
+
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+public class HamcrestMatchersApiTest {
+
+      /*
+       given accept type is Json
+       And path param id is 15
+       When user sends a get request to spartans/{id}
+       Then status code is 200
+       And content type is Json
+       And json data has following
+           "id": 15,
+           "name": "Meta",
+           "gender": "Female",
+           "phone": 1938695106
+        */
+
+    @DisplayName("OneSpartan with Hamcrest and chaining")
+    @Test
+    public void test1(){
+
+        given().
+                accept(ContentType.JSON)
+                .pathParams("id",15)
+                .when()
+                .get("http://44.204.1.124:8000/api/spartans/{id}")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType("application/json")
+                .body("id",equalTo(15),
+                        "name",equalTo("Meta"),
+                "gender",is(equalTo("Female")),
+                "phone",is(1938695106));
+
+
+
+
+    }
+
+
+}
