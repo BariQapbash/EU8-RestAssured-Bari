11de7526e90977f365c0d56e87fe824d16109b6b
BariQapbash <99431864+BariQapbash@users.noreply.github.com>
2022-07-25 15:02:46 +0200

    day05 continue

--- src/test/java/com/cybertek/day05/HandCrestMatchersIntro.java
+++ src/test/java/com/cybertek/day05/HandCrestMatchersIntro.java
@@ -1,16 +1,18 @@
 package com.cybertek.day05;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class HandCrestMatchersIntro {
 
     @Test
     public void test1(){
 
-
-
-
-
+        assertThat(5+5, Matchers.is(10));
     }
 
 
