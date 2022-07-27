6cc83d53b7383223728e6b8a60ffddfdd1ab0a7e
BariQapbash <99431864+BariQapbash@users.noreply.github.com>
2022-07-27 23:30:19 +0200

    day06 continue

--- src/test/java/com/cybertek/day06/ORDSPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day06/ORDSPojoGetRequestTest.java
@@ -1,5 +1,6 @@
 package com.cybertek.day06;
 
+import com.cybertek.pojo.Employee;
 import com.cybertek.pojo.Link;
 import com.cybertek.pojo.Region;
 import com.cybertek.utilities.HRTestBase;
@@ -20,19 +21,36 @@ public class ORDSPojoGetRequestTest extends HRTestBase {
     @Test
     public void regionTest(){
 
-        JsonPath jsonPath = get("/regions").then().statusCode(200).extract().jsonPath();
+        JsonPath jsonPath = get("/regions").then().statusCode(200).log().body().extract().jsonPath();
 
         Region region1 = jsonPath.getObject("items[0]", Region.class);
 
         System.out.println(region1);
 
-        System.out.println("region1.getRegion_id() = " + region1.getRegion_id());
+        System.out.println("region1.getRegion_id() = " + region1.getRId());
         System.out.println("region1.getRegion_name() = " + region1.getRegion_name());
         System.out.println("region1.getLinks().get(0).getHref() = " + region1.getLinks().get(0).getHref());
 
+    }
 
+    @DisplayName("GET request to /employees and only get couple of values as a Pojo class")
+    @Test
+    public void employeeGet(){
+
+        Employee employee1 = get("/employees").then().statusCode(200)
+                .extract().jsonPath().getObject("items[0]", Employee.class);
+
+        System.out.println(employee1);
 
     }
+
+    /* send a get request to regions
+        verify that region ids are 1,2,3,4
+        verify that regions names Europe ,Americas , Asia, Middle East and Africa
+        verify that count is 4
+        try to use pojo as much as possible
+        ignore non used fields
+     */
 
 
 }
