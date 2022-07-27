dc9ca6a87af4596eefe14662a152ca1ee23e3df3
BariQapbash <99431864+BariQapbash@users.noreply.github.com>
2022-07-27 22:13:37 +0200

    day06 continue

--- src/test/java/com/cybertek/pojo/Spartan.java
+++ src/test/java/com/cybertek/pojo/Spartan.java
@@ -1,5 +1,10 @@
 package com.cybertek.pojo;
 
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 /*
 {
 "id": 15
@@ -10,6 +15,12 @@ package com.cybertek.pojo;
 }
  */
 
+
+
+@Getter
+@Setter
+@ToString
+
 public class Spartan {
 
 
@@ -18,46 +29,5 @@ public class Spartan {
     private String gender;
     private long phone;
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public long getPhone() {
-        return phone;
-    }
-
-    public void setPhone(long phone) {
-        this.phone = phone;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Spartan{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", gender='" + gender + '\'' +
-                ", phone=" + phone +
-                '}';
-    }
 }
